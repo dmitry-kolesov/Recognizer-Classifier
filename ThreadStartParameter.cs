243bec5962ca7c5f5f923242b87bb4daa67d6336
kdb <d.kolesov@osgrm.ru>
2019-11-05 15:15:18 +0700

    delete extra

--- ThreadStartParameter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TesseractPatagamesTest
-{
-    class ThreadStartParameter
-    {
-        public string Path { get; set; }
-        public int StartIndex { get; set; }
-        public int EndIndex { get; set; }
-    }
-}
